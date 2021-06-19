7389996398df713819e781b89abd496eab89cc55
Sam Vivonia <snvivonia@gmail.com>
2021-06-19 00:12:35 -0700

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,19 @@
+// create your loops here.
+
+console.log('LOOP 1:');
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log('LOOP 2:');
+
+for (let i = 0; i < 10; i++) {
+  console.log(i * 2);
+}
+
+console.log('LOOP 3:');
+
+for (let i = 100; i > -1; i--) {
+  console.log('Time til explosion: ' + i);
+}
